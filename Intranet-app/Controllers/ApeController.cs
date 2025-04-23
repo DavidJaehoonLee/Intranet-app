34282f652a849d1ff6b888e783bc5e01a8443fb9
David Lee <david.lee@amwestfunding.com>
2025-04-23 16:50:14 -0700

    Db setup with PostgreSQL and some models

---
+++ Intranet-app/Controllers/ApeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intranet_app.Controllers
+{
+    public class ApeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Intranet-app/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intranet_app.Models
+{
+    public class Recipe
+    {
+        public int RecipeID { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeDescription { get; set; }
+
+    }
+}
