a71e3074210ccecd7e1512c8bec0b2d6f6365c68
David Lee <david.lee@amwestfunding.com>
2025-05-12 09:10:45 -0700

    message.js added

---
+++ Intranet-app/Scripts/formControl.js
@@ -0,0 +1,18 @@
+﻿
+
+export default class FormControl {
+    #decimals = 0;
+    #numberMask = false;
+    #value = "";
+    #emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    constructor(id, decimals = 0, maxLength = 0) {
+        super(document.getElementById(id).parentNode);
+        this.elem = document.getElementById(id);
+        this.#decials = decimals;
+        if (this.elem.nodeName === "INPUT" && this.elem.getAttribute("inputmode") === "numberic") {
+
+        }
+
+    }
+
+}
---
+++ Intranet-app/Scripts/message.js
@@ -0,0 +1,42 @@
+﻿export const MessageType = Object.freeze({
+    ERROR: "error",
+    SUCCESS: "success",
+    WARNING: "warning",
+    INFO: "info",
+    CONFIRM: "confirm"
+});
+
+export default class Message {
+    constructor(container) {
+        this.container = container;
+        this.message = "";
+    }
+
+    #setMessage(type, message) {
+        this.container.dataset.type = type;
+        this.container.dataset.msg = message;
+        this.message = message;
+    }
+
+    setErrorMessage(message) {
+        this.#setMessage(MessageType.ERROR, message);
+    }
+
+    setWarningMessage(message) {
+        this.#setMessage(MessageType.WARNING, message);
+    }
+
+    setInfoMessage(message) {
+        this.#setMessage(MessageType.INFO, message);
+    }
+
+    setSuccessMessage(message) {
+        this.#setMessage(MessageType.SUCCESS, message);
+    }
+
+    clearMessage() {
+        delete this.container.dataset.type;
+        delete this.container.dataset.msg;
+        this.message = "";
+    }
+}
