c07faf13f7c1132da1250ae75d9e27d6e2e1f89f
David Lee <david.lee@amwestfunding.com>
2025-05-29 11:33:28 -0700

    custom javascript scripts

---
+++ Intranet-app/Scripts/Modules/utils.js
@@ -0,0 +1,13 @@
+﻿export default class Utils {
+    static removeHtmlTags(string) {
+        let result = string.replace(/<\/?[^>]+(>|$)/g, ""); // Remove Html Tags
+        result = result.replace(/&(?!gt;|lt;)[^;]+;/g, ""); // Remove HTML entities except &gt; &lt; because those 2 are required
+        result = result.replace("&gt;", ">").replace("&lt;", "<"); // replace &gt; for > and &lt for <
+        return result;
+    }
+
+    static reloadCurrentPage(secondsToWaitBeforeReload = 5) {
+        const miliseconds = Number(secondsToWaitBeforeReload) * 1000;
+        setTimeout(() => window.location.reload(), miliseconds);
+    }
+}
