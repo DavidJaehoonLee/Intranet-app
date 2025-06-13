148d2f3ae1da3e54c52ee07ee267861632e75508
David Lee <david.lee@amwestfunding.com>
2025-06-13 16:28:40 -0700

    form control fields

---
+++ Intranet-app/wwwroot/js/Views/formControlFields.js
@@ -0,0 +1,13 @@
+﻿import FormControl from "../Modules/formControl.js";
+
+const txtRequested = new FormControl("txtRequested");
+const cmbForward = new FormControl("cmbForward");
+const txtForwardEmail = new FormControl("txtForwardEmail");
+
+const field = {
+    txtRequested,
+    cmbForward,
+    txtForwardEmail
+}
+
+export default field;
